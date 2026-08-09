493d55293abd2df7b98166766027e645e5085665
Andrew Ramo <298294352+andrew-ramo@users.noreply.github.com>
2026-08-09 05:28:17 +0300

    feat: build product details page with dynamic routing

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Container } from "@/components/layout/container";
 import { getProductById } from "@/features/products/services/get-products";
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -11,9 +14,25 @@ export default async function ProductPage({ params }: Props) {
 
   if (product === null) notFound();
   return (
-    <div>
-      name : {product.name} <br />
-      price : {product.price}
-    </div>
+    <Container>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="relative aspect-square">
+          <Image
+            src={product.image_url}
+            alt={product.name}
+            fill
+            sizes="(max-width: 1024px) 100vw, 50vw"
+          />
+        </div>
+        <div className="flex flex-col gap-6">
+          <h1 className="text-2xl">{product.name}</h1>
+          <div className="flex gap-4">
+            <p className="text-xl font-bold">{product.price} EGP</p>
+            <Badge>{product.category}</Badge>
+          </div>
+          <p className="text-base">{product.description}</p>
+        </div>
+      </div>
+    </Container>
   );
 }
